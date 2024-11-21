1f93b584f4bbce3b8894119b16e81fdd50bcbacf
Enzo Sandre <enzo@enzosandre.fr>
2024-11-21 15:36:23 +0100

    [REFACTO] Fusion des tests

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -5,24 +5,16 @@ from ohce import Ohce
 
 class MyTestCase(unittest.TestCase):
     def test_miroir(self):
-        # ETANT DONNE une chaîne
-        chaîne = 'test'
+        cas = ['test', 'ynov']
 
-        # QUAND on la saisit
-        résultat = Ohce.saisir(chaîne)
+        for chaîne in cas:
+            # ETANT DONNE une chaîne
+            with self.subTest(chaîne):
+                # QUAND on la saisit
+                résultat = Ohce.saisir(chaîne)
 
-        # ALORS elle est renvoyée en miroir
-        self.assertEqual('tset', résultat)
-
-    def test_miroir_2(self):
-        # ETANT DONNE une chaîne
-        chaîne = 'ynov'
-
-        # QUAND on la saisit
-        résultat = Ohce.saisir(chaîne)
-
-        # ALORS elle est renvoyée en miroir
-        self.assertEqual('vony', résultat)
+                # ALORS elle est renvoyée en miroir
+                self.assertEqual(chaîne[::-1], résultat)
 
 
 if __name__ == '__main__':
