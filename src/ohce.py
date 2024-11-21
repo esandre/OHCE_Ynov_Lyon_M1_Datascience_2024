f0b1d8065f5c3c4a428380d136dbbb61566cd0fe
Enzo Sandre <enzo@enzosandre.fr>
2024-11-21 15:45:59 +0100

    [GREEN]

--- src/ohce.py
+++ src/ohce.py
@@ -4,4 +4,6 @@ import os
 class Ohce:
     @classmethod
     def saisir(cls, chaîne):
-        return chaîne[::-1] + os.linesep + 'Bien dit !'
+        return chaîne[::-1] + os.linesep + 'Bien dit !' \
+            if chaîne == 'radar' \
+            else chaîne[::-1]
